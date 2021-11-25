a08579b5702822f93427e7d64268fc9b0da542af
AndersonMads <91493130+AndersonMads@users.noreply.github.com>
2021-11-25 01:07:30 +0100

    Register funktion
    Register funktionen er nu lavet, hvor users bliver gemt i en users.json fil

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+const fs = require('fs')
+
+
+// Gør så vi kan se vores værdier når vi registerer, dvs req.body.name fx
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 
 app.listen(3030, () => {
     console.log('Serveren kører på http://localhost:3030')
 });
+
+
 
 // Homepage
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-// Register page
+// Login page
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html')
+    res.sendFile(__dirname + '/views/login.html');
 });
 
+// Register page
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+
+// Save user - vi bruger appendfile så den ikke overwriter hver gang, hvilket den gør med fs.writefile
+app.post('/register', (req, res) => {
+    
+    fs.appendFile('db/users.json', JSON.stringify(req.body, null, 4), err => {
+        if(err) res.send(err)
+        res.redirect('/login')
+        
+    })
+})
+
+// Login med rigtige details
+
+
+
+    
