75aefce102a36ccd28df234b7af3316e0cbde4f1
AndersonMads <91493130+AndersonMads@users.noreply.github.com>
2021-11-28 23:09:53 +0100

    Delete og opdater bruger
    Nu er både delete user og opdater user (med password og email) lavet.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 const fs = require('fs')
@@ -32,6 +33,11 @@ app.get('/delete', (req, res) => {
     res.sendFile(__dirname + '/views/delete.html')
 });
 
+// Opdater bruger page
+app.get('/update', (req, res) => {
+    res.sendFile(__dirname + '/views/update.html');
+});
+
 
 // Save user når man registrerer - vi laver først og fremmest users.json til et array så vi kan bruge array funktioner som .push
 // Desuden er det nyttigt at vores users.json er et array senere hen, fordi vi så kan søge i det
@@ -49,15 +55,48 @@ app.post('/register', (req, res) => {
 app.post('/login', (req, res) => {
     const registeredUsers = fs.readFileSync('db/users.json');
     const authenticatedUsers = JSON.parse(registeredUsers)
-    var nej = false;
-
+    
     for (let i = 0; i < authenticatedUsers.length; i++)
         if (authenticatedUsers[i].email == req.body.email && authenticatedUsers[i].password == req.body.password){
             res.redirect('/')        
         }});
 
-
-   
-
-
+// Slet bruger - basicallly præcis samme fremgangsmåde som login, bare med en splice array method som sletter vores input data
+app.delete('/delete', (req, res) => {
+    const registeredUsers = fs.readFileSync('db/users.json');
+    const authenticatedUsers = JSON.parse(registeredUsers)
     
+    for (let i = 0; i < authenticatedUsers.length; i++)
+        if (authenticatedUsers[i].email == req.body.email){
+            authenticatedUsers.splice(i, 1)
+            res.send('Brugeren er nu slettet, gå til http://localhost:3030 for at fortsætte')
+        } else {
+            res.send('Brugeren findes ikke, gå til http://localhost:3030 for at fortsætte')
+        }       
+        
+        fs.writeFile('db/users.json', JSON.stringify(authenticatedUsers, null, 4), err =>{
+            if(err) res.send(err)
+        })   
+        });
+
+
+// Opdater bruger - her kan du enten opdatere email eller password, derfor et else if statement.
+app.put('/update', (req, res) => {
+    const registeredUsers = JSON.parse(fs.readFileSync('db/users.json'));
+
+    for (let i = 0; i < registeredUsers.length; i++) {
+        if(registeredUsers[i].email == req.body.email) {
+            registeredUsers[i].email = req.body.nyemail
+            res.send('Emaillen er nu ændret, gå til http://localhost:3030 for at fortsætte')
+        } else if(registeredUsers[i].password == req.body.password) {
+            registeredUsers[i].password = req.body.nytpassword
+            res.send('Password er nu ændret, gå til http://localhost:3030 for at fortsætte')
+        } else {
+            res.send('Kunne ikke finde de indtastede oplysninger, gå til http://localhost:3030 for at fortsætte')
+        }
+        fs.writeFile('db/users.json', JSON.stringify(registeredUsers, null, 4), err => {
+            if(err) res.send(err)
+        })
+    }
+})
+
