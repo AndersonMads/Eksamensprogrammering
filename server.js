8aa22e5fc1101ac4afa2f1f3f9f91612be607f2b
AndersonMads <91493130+AndersonMads@users.noreply.github.com>
2021-11-28 21:20:42 +0100

    Login knap
    Nu er login knappet lavet, så man skal logge ind med de rigtige details for at komme videre på siden

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser')
 const fs = require('fs')
-
+const methodOverride = require('method-override'); // Dette gør at vi kan lave DELETE og PUT request i vores HTML Forms!
+app.use(methodOverride('_method'));
 
 // Gør så vi kan se vores værdier når vi registerer, dvs req.body.name fx
 app.use(express.json());
@@ -12,15 +12,12 @@ app.listen(3030, () => {
     console.log('Serveren kører på http://localhost:3030')
 });
 
-
-
-// Homepage
+// Homepage - res.sendFile(__dirname) gør at vi kan render html'en
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html'); 
 });
 
 // Login page
-
 app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
@@ -30,19 +27,37 @@ app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 });
 
+// Slet bruger page
+app.get('/delete', (req, res) => {
+    res.sendFile(__dirname + '/views/delete.html')
+});
 
-// Save user - vi bruger appendfile så den ikke overwriter hver gang, hvilket den gør med fs.writefile
+
+// Save user når man registrerer - vi laver først og fremmest users.json til et array så vi kan bruge array funktioner som .push
+// Desuden er det nyttigt at vores users.json er et array senere hen, fordi vi så kan søge i det
 app.post('/register', (req, res) => {
-    
-    fs.appendFile('db/users.json', JSON.stringify(req.body, null, 4), err => {
+
+    let userDB = JSON.parse(fs.readFileSync('db/users.json'))
+    userDB.push(req.body) // Her pusher vi req.body(i dette tilfælde inputtet i register form'en) til vores 'database' 
+    fs.writeFile('db/users.json', JSON.stringify(userDB, null, 4), err => {
         if(err) res.send(err)
-        res.redirect('/login')
-        
+        res.redirect('/login') // Efter man har registreret sin bruger bliver man redirectet til login page
     })
 })
 
-// Login med rigtige details
+// Login med de rigtige details - mangler at lave et else statement, hvis details fx ikke passer
+app.post('/login', (req, res) => {
+    const registeredUsers = fs.readFileSync('db/users.json');
+    const authenticatedUsers = JSON.parse(registeredUsers)
+    var nej = false;
 
+    for (let i = 0; i < authenticatedUsers.length; i++)
+        if (authenticatedUsers[i].email == req.body.email && authenticatedUsers[i].password == req.body.password){
+            res.redirect('/')        
+        }});
+
+
+   
 
 
     
